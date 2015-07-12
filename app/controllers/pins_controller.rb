9fb91f5b215cc9a996c105271e8606d7e753162e
Tom Schillemans <tomschillemans@gmail.com>
2015-07-12 10:13:20 +0200

    Created user/pin association, add devise, and styled pages using materialize-sass

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,52 @@
+class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all.order('created_at DESC')
+  end
+
+  def show
+    # Handled by the before action
+  end
+
+  def new
+    @pin = current_user.pins.build
+  end
+
+  def create
+    @pin = current_user.pins.(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: 'Successfully create new Pin'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    # Handled by the before action
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+end
