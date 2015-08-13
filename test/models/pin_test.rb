63f38c05b0b307feb96d2430f0520094ca4f3c4d
Tom Schillemans <tomschillemans@gmail.com>
2015-08-13 16:41:17 +0200

    added tests (not working yet)

--- test/models/pin_test.rb
+++ test/models/pin_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test 'pin should not save without a title' do
+  #   pin = Pin.new
+  #   assert_not pin.save, 'Pin saved without a title is: TRUTHY, should be: FALSY'
   # end
 end
